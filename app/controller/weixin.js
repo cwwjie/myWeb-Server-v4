2211ba8ba8dafaf30b980cf9b33422a41dcacedb
Rejiejay <454766952@qq.com>
2018-12-14 10:54:36 +0800

    creat wx config

--- app/controller/weixin.js
+++ app/controller/weixin.js
@@ -5,7 +5,7 @@ const lodash = require('lodash');
 // 组件类
 const consequencer = require('./../utils/consequencer');
 const encryption = require('./../utils/encryption');
-const getjsonby = require('./../utils/getjsonbyhttps');
+const createRandomStr = require('./../utils/createRandomStr');
 
 class WeixinController extends Controller {
     async index() {
@@ -143,6 +143,77 @@ class WeixinController extends Controller {
      */
     async getJsApi_ticket() {
         this.ctx.body = await this.ctx.service.weixin.getJsApi_ticket();
+    }
+
+    /**
+     * 通过 url(当前网页的URL，不包含#及其后面部分) 获取公众号用于调用微信JS接口的配置信息
+     * 配置信息:
+     * 1. noncestr 随机字符串
+     * 2. jsapi_ticket 公众号用于调用微信JS接口的临时票据
+     * 3. timestamp 时间戳 精确到 秒 就是 new Date().getTime() / 1000
+     * 4. signature 签名 sha1加密的签名
+     * 文档： 附录1-JS-SDK使用权限签名算法 https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421141115
+     * 微信 JS 接口签名校验合法性工具 https://mp.weixin.qq.com/debug/cgi-bin/sandbox?t=jsapisign
+     */
+    async getWxConfig() {
+        // 判断 是否不存在 url 参数
+        if (!this.ctx.request.query || !this.ctx.request.query.url) {
+            return this.ctx.body = consequencer.error('参数错误!');
+        }
+
+        // 如果存在 url ，校验 url 的合法性
+        let my_url = decodeURIComponent(this.ctx.request.query.url);
+        if ( /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/.test(my_url) === false) {
+            return this.ctx.body = consequencer.error('url参数不合法!');
+        }
+
+        // 去除掉 # 及其后面部分
+        my_url = my_url.split('#')[0];
+
+        // 获取 jsapi_ticket 公众号用于调用微信JS接口的临时票据
+        let awaitJsapi_ticket = await this.ctx.service.weixin.getJsApi_ticket();
+
+        // 判断获取的 jsapi_ticket 是否有误
+        if (awaitJsapi_ticket.result !== 1) {
+            return this.ctx.body = consequencer.error(`获取公众号用于调用微信JS接口的临时票据有误, 原因: ${awaitJsapi_ticket.message}`);
+        }
+
+        let jsapi_ticket = awaitJsapi_ticket.data;
+        
+        // 随机 生成签名的15位长的随机串
+        let nonceStr = createRandomStr(15); 
+
+        // 生成 timestamp 时间戳 精确到 秒
+        let timestamp = `${new Date().getTime() / 1000}`; // 必须字符串
+
+        /**
+         * 对所有待签名参数按照字段名的 ASCII 码从小到大排序（字典序）后，使用URL键值对的格式（即key1=value1&key2=value2…）拼接成字符串string1
+         */
+        // 需要进行 ASCII 码从小到大排序（字典序）的对象
+        let sortObj = { 
+            jsapi_ticket: jsapi_ticket,
+            noncestr: nonceStr, // 所有参数名均为小写字符
+            timestamp: timestamp,
+            url: my_url
+        }
+        // ASCII 码从小到大排序（字典序）后的数组
+        let asciiSortArry = Object.keys(sortObj).sort(); // 先用Object内置类的keys方法获取要排序对象的属性名数组 再利用Array的sort方法进行排序
+        
+        // 遍历 ASCII 码从小到大排序（字典序）
+        let asciiSortUrlString = ''; // 创建一个新的字符串，用于存放排好序的URL键值对的格式（即key1=value1&key2=value2…）
+        for (var i = 0; i < asciiSortArry.length; i++) {
+            asciiSortUrlString += `${[asciiSortArry[i]]}=${sortObj[asciiSortArry[i]]}${i === (asciiSortArry.length - 1) ? '' : '&'}`
+        }
+
+        // sha1加密
+        let signature = encryption.sha1ToLowerCase(asciiSortUrlString);
+
+        this.ctx.body = consequencer.success({
+            appId: this.config.weixin.appID,
+            timestamp: timestamp,
+            nonceStr: nonceStr,
+            signature: signature
+        });
     }
 }
 
--- app/router.js
+++ app/router.js
@@ -35,11 +35,7 @@ module.exports = app => {
     router.get('/weixin', controller.weixin.index); // 测试
     router.get('/weixin/handle', controller.weixin.responseHandle); // 微信验证开发者服务器
     router.post('/weixin/handle', controller.weixin.messageHandle); // 文本消息
-    // 只有在本地测试的时候，这个几个API才进行监听
-    // if (app.config.env === 'local') {
-        router.get('/weixin/get/global_access_token', controller.weixin.getGlobalAccess_token); // 获取公众号的全局唯一接口调用凭据
-        router.get('/weixin/get/jsapi_ticket', controller.weixin.getJsApi_ticket); // 获取公众号用于调用微信JS接口的临时票据
-    // }
+    router.get('/weixin/get/jssdkconfig', controller.weixin.getWxConfig); // 获取公众号用于调用微信JS接口的配置信息
 
     // 百度
     router.get('/baidu', controller.baidu.index); // 测试
